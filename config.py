fe2088cd76a45cf3df22f38cd66a584100f9bf48
AnelD <anel.dervisic@gmail.com>
2025-04-21 10:07:47 +0200

    create config

---
+++ config.py
@@ -0,0 +1,20 @@
+WEBSOCKET_URI = "ws://localhost:8080"
+
+# Directory where audio files with patient data are uploaded
+DATA_AUDIO_DIR = ""
+
+# Directory where audio files with voice commands are uploaded
+COMMAND_AUDIO_DIR = ""
+
+
+# Variables for TTS
+# Directory where your downloaded piper.exe is
+PIPER_DIR = "/home/anel/Desktop/piper"
+
+# Name of the voice model you want to use for tts
+# If it's not inside the piper_dir give the full path
+VOICE_NAME = "de_DE-thorsten-high.onnx"
+
+# Where the generated audio will be saved to,
+# for relative paths keep in mind that it starts in piper_dir
+GENERATE_AUDIO_DIR = "/home/anel/PycharmProjects/speech_recognition/data/generated"
