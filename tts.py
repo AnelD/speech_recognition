daf4d5d8aa8b73ce211df6a043e8f42e1cebf7e3
AnelD <anel.dervisic@gmail.com>
2025-04-21 10:08:44 +0200

    run tts in separate thread

--- tts.py
+++ tts.py
@@ -1,13 +1,23 @@
 import asyncio
+import os
+from pathlib import Path
+
+import config
 
 
-async def run_command_in_subprocess(command: str, cwd):
-    # Use a subprocess shell on Windows.
+async def run_command_in_subprocess(command: str, cwd: str):
+    """
+    Run a command in a subprocess shell, it awaits the command execution.
+
+    :param command: command to run
+    :param cwd: directory to run the command in
+    :return: None, prints stdout and stderr to console
+    """
     proc = await asyncio.create_subprocess_shell(
         command,
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE,
-        shell=True,  # shell should be True to invoke the command shell.
+        shell=True,
         cwd=cwd,
     )
 
@@ -22,20 +32,28 @@ async def run_command_in_subprocess(command: str, cwd):
 
 
 async def get_audio(queue):
-    # piper_path = r"C:\Users\dervi\Desktop\piper"
-    piper_path = r"/home/anel/Desktop/piper"
-    piper_command = r"| ./piper "
-    modell_hessisch = "-m Thorsten-Voice_Hessisch_Piper_high-Oct2023.onnx "
-    modell_normal = "-m de_DE-thorsten-high.onnx "
-    # output_path = r"-d C:\Users\dervi\PycharmProjects\speechPoc\data\generated"
-    output_path = r"-d /home/anel/PycharmProjects/speech_recognition/data/generated"
+    """
+    Turn a text message into an audio file.
+
+    :param queue: asyncio.Queue from which to get the text
+    :return: None
+    """
+    piper_dir = str(Path(config.PIPER_DIR.encode("unicode_escape").decode()))
+    piper_command = rf"| .{os.sep}piper "
+    voice = "-m " + config.VOICE_NAME
+    output_path = "-d " + str(
+        Path(config.GENERATE_AUDIO_DIR.encode("unicode_escape").decode())
+    )
+    loop = asyncio.get_event_loop()
 
     while True:
-        input = await queue.get()
-        input_text = f'echo "{input}" '
+        text = await queue.get()
+        input_text = f'echo "{text}" '
         print(input_text)
-        command = input_text + piper_command + modell_normal + output_path
+        command = input_text + piper_command + voice + output_path
         print(command)
 
-        # Run the command in a separate thread using asyncio.to_thread
-        await asyncio.create_task(run_command_in_subprocess(command, piper_path))
+        # Run the generation in separate thread to not block the event loop
+        await loop.run_in_executor(
+            None, run_command_in_subprocess, *(command, piper_dir)
+        )
